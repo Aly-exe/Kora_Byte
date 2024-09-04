19f3515b8cf364431e9cb167112790a2ea536e48
Aly-exe <alibnraslan@gmail.com>
2024-09-05 01:52:11 +0200

    Make CustomScrollView

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:beautiful_soup_dart/beautiful_soup.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -16,52 +15,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xff2412C0),Color(0xff4910BC)], // Define your gradient colors
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff2412C0),
+                  Color(0xff4910BC)
+                ], // Define your gradient colors
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
             ),
-          ),
-          child: AppBar(
-            backgroundColor: Colors.transparent,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
                     "assets/images/korabytelogo.png",
                     height: 50,
                     fit: BoxFit.cover,
-                    
-                ),
-                Text('Korabyte',
-                style: TextStyle(fontSize: 20, color: Colors.white),),
-              ],
+                  ),
+                  Text(
+                    'Korabyte',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ],
+              ),
+              centerTitle: true,
             ),
-            centerTitle: true,
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          MatchesWidget(),
-          SourcesListViewWidget(),
+        body: CustomScrollView(physics: BouncingScrollPhysics(), slivers: [
+          SliverToBoxAdapter(
+            child: MatchesWidget(),
+          ),
+          SliverToBoxAdapter(
+            child: SourcesListViewWidget(),
+          ),
           NewsCardWidget()
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: () async {
-        // await DioHelper.getData(Constants.filGaoal).then((value) {
-        //   var body =BeautifulSoup(value.data).find("div" , class_:"main_body" )!.find("div" , id: "list-box")!.findAll("li");
-        //   body.forEach((e) {
-        //     log(e.find('h6')!.text.toString().trim());
-        //     log(e.find('a')!.attributes['href'].toString());
-        //     log(e.find('img')!.attributes['data-src'].toString());
-        //   });
-        // }).catchError((error) {});
-      }),
-    );
+        ]));
   }
 }

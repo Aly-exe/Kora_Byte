fbc8466277161fc9302919dfd7b3d47f520809e3
Aly-exe <alibnraslan@gmail.com>
2024-09-05 01:43:30 +0200

    Add screenUtilInit to make App responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kora_news/screens/homescreen.dart';
 import 'package:kora_news/services/get_news_bloc.dart';
 import 'package:kora_news/shared/dio_helper.dart';
@@ -18,10 +19,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => GetNewsBloc()..getFilgoalNews()..getMatches(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Kora News',
-        home: HomeScreen(),
+      child: ScreenUtilInit(
+        designSize: const Size(360, 756),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder:(context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          locale: Locale('ar'),
+          title: 'Kora News',
+          home: HomeScreen(),
+        );
+      }
       ),
     );
   }

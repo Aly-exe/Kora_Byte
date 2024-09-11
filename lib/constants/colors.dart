896654cfc284ed5b7a460a60c09c88b42f5bda75
Aly-exe <alibnraslan@gmail.com>
2024-09-11 19:13:25 +0200

    add linear gradient Constant

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,5 +1,15 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class ColorPallet{
   static final Color kNavyColor=Color(0xff190159);
+  static final linearGradient = LinearGradient(
+          colors: [
+            Color(0xff2412C0),
+            Color(0xff4910BC)
+          ], 
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        );
 }

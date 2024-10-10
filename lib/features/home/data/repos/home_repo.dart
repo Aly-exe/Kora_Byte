a63bea788eace97172e9910b7e637cc3248acd5d
Aly-exe <alibnraslan@gmail.com>
2024-10-10 12:27:19 +0200

    finish Get News method

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:kora_news/features/home/data/models/filgoal_news_model.dart';
+
 abstract class HomeRepo{
   Future getMatches({String? url});
   Future getMatchDetails(String url);
-  Future getNews();
+  Future<List<FilgoalNewsModel>> getNews({int? index});
   Future getNewsDetails();
 }
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -5,17 +5,23 @@ import 'package:kora_news/core/constants/constants.dart';
 import 'package:kora_news/core/functions/arrange_matches_list.dart';
 import 'package:kora_news/core/functions/collect_match_data.dart';
 import 'package:kora_news/core/functions/collect_match_details_data.dart';
+import 'package:kora_news/core/functions/get_btolat_news.dart';
+import 'package:kora_news/core/functions/get_epl_news.dart';
+import 'package:kora_news/core/functions/get_filgoal_news.dart';
+import 'package:kora_news/core/functions/get_korablus_news.dart';
+import 'package:kora_news/core/functions/get_yallakora_news.dart';
 import 'package:kora_news/core/helpers/dio_helper.dart';
+import 'package:kora_news/features/home/data/models/filgoal_news_model.dart';
 import 'package:kora_news/features/home/data/models/match_model.dart';
 import 'package:kora_news/features/home/data/repos/home_repo.dart';
 
 class HomeRpeoImplementation implements HomeRepo {
   @override
   Future getMatchDetails(String url) async {
-    try{
+    try {
       Response response = await DioHelper.getData(url);
       return scrapingMatchDetails(response.data);
-    }catch(error){
+    } catch (error) {
       return Future.error(error);
     }
   }
@@ -23,23 +29,37 @@ class HomeRpeoImplementation implements HomeRepo {
   @override
   Future getMatches({String? url}) async {
     List<Matches> matchesList = [];
-    var matcheslink =Uri.decodeFull(url ?? Constants.yallaKoraMatches);
-    try{
-    var response = await DioHelper.getData(matcheslink);
+    var matcheslink = Uri.decodeFull(url ?? Constants.yallaKoraMatches);
+    try {
+      var response = await DioHelper.getData(matcheslink);
       fillMatchesList(response, matchesList);
       arrangeMatchesList(matchesList);
       return matchesList;
-
-    }catch(error){
+    } catch (error) {
       print("Get Matches List error : $error");
       return error;
     }
   }
 
   @override
-  Future getNews() {
-    // TODO: implement getNews
-    throw UnimplementedError();
+  Future<List<FilgoalNewsModel>> getNews({int? index}) async {
+    List<FilgoalNewsModel> newsList = [];
+    try {
+      if (index == 0) {
+        newsList = await getEplNews();
+      } else if (index == 2) {
+        newsList = await getYallaKoraNews();
+      } else if (index == 3) {
+        newsList = await getKoraPlusNews();
+      } else if (index == 4) {
+        newsList = await getBtolatNews();
+      } else {
+        newsList = await getFilGoalNews();
+      }
+      return newsList;
+    } catch (error) {
+      return Future.error(error);
+    }
   }
 
   @override

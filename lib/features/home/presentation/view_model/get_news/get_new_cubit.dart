e6aaabb94bdb2f4ce38c82dc359afce3c042101c
Aly-exe <alibnraslan@gmail.com>
2024-10-11 07:03:23 +0200

    handle getNewsDetails in cubit

--- lib/features/home/presentation/view_model/get_news/get_new_cubit.dart
+++ lib/features/home/presentation/view_model/get_news/get_new_cubit.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kora_news/features/home/data/models/filgoal_news_model.dart';
+import 'package:kora_news/features/home/data/models/news_details_model.dart';
+import 'package:kora_news/features/home/data/models/news_model.dart';
 import 'package:kora_news/features/home/data/repos/home_repo_implementation.dart';
 import 'package:kora_news/features/home/presentation/view_model/get_news/get_news_states.dart';
 
@@ -9,16 +9,26 @@ class GetNewsCubit extends Cubit<GetNewsStates>{
   GetNewsCubit() : super(InitialGetNewsState());
   static GetNewsCubit get(context) => BlocProvider.of(context);
   HomeRpeoImplementation homeRepo = HomeRpeoImplementation();
-  List<FilgoalNewsModel> newsList=[];
+  List<NewsModel> newsList=[];
+  NewsDetailsModel newsDetails=NewsDetailsModel();
   Future getNews({int?index})async{
     emit(LoadingGetNewsState());
     try{
       newsList=await homeRepo.getNews(index: index);
-      log(newsList[0].title??"No Title Get");
       emit(SuccessGetNewsState());
     }catch(error){
       log(error.toString());
       emit(FailedGetNewsState());
+    }
+  }
+  Future getNewsDetails({required String baseUrl ,required String url})async{
+    emit(LoadingGetNewsDetailsState());
+    try{
+      newsDetails=await homeRepo.getNewsDetails(baseUrl: baseUrl, url: baseUrl+url);
+      emit(SuccessGetNewsDetailsState());
+    }catch(error){
+      log("Cubit Error $error");
+      emit(FailedGetNewsDetailsState());
     }
   }
 }
--- lib/features/home/presentation/view_model/get_news/get_news_states.dart
+++ lib/features/home/presentation/view_model/get_news/get_news_states.dart
@@ -3,3 +3,6 @@ class InitialGetNewsState extends GetNewsStates{}
 class LoadingGetNewsState extends GetNewsStates{}
 class SuccessGetNewsState extends GetNewsStates{}
 class FailedGetNewsState extends GetNewsStates{}
+class LoadingGetNewsDetailsState extends GetNewsStates{}
+class SuccessGetNewsDetailsState extends GetNewsStates{}
+class FailedGetNewsDetailsState extends GetNewsStates{}
